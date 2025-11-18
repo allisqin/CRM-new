a587a78d99fc156db7db53603f99b8b415d8bab0
Allisqin <allisqin@gmail.com>
2025-11-18 11:19:19 +0300

    Добавлена проверка на ввод пользователя.

--- script.js
+++ script.js
@@ -8,17 +8,17 @@
 
 // console.log(quantityGood * priceGoog);
 
-// const nameGoog = prompt("Введите наименование товара");
-// const categoryCoog = prompt('Введите категорию товара');
-// let quantityGood = Number(prompt('Введите количество товара'));
-// let priceGood = Number(prompt('Введите цену товара'));
-// if (isNaN(quantityGood) || isNaN(priceGood)) {
-//    console.log('Вы ввели некорректные данные');
-// }
-// else {
-//    console.log(typeof quantityGood);
-//    console.log(typeof priceGood);
-//    console.log(`На складе ${quantityGood} единицы товара ${nameGoog} на сумму ${quantityGood * priceGood} долларов`);
-// }
+const nameGoog = prompt("Введите наименование товара");
+const categoryCoog = prompt('Введите категорию товара');
+let quantityGood = Number(prompt('Введите количество товара'));
+let priceGood = Number(prompt('Введите цену товара'));
+if (isNaN(quantityGood) || isNaN(priceGood)) {
+   console.log('Вы ввели некорректные данные');
+}
+else {
+   console.log(typeof quantityGood);
+   console.log(typeof priceGood);
+   console.log(`На складе ${quantityGood} единицы товара ${nameGoog} на сумму ${quantityGood * priceGood} долларов`);
+}
 
 
--- tasks.js
+++ tasks.js
@@ -1,5 +1,7 @@
 'use sctrict'
 
+
+// Дождь
 const rain = Math.floor(Math.random());
 
 if (rain === 0) {
@@ -9,18 +11,18 @@ if (rain === 0) {
 }
 
 // Институт
-// const mat = Number(prompt('Введите количество баллов по математике'));
-// const rus = Number(prompt('Введите количество баллов по русскому языку'));
-// const info = Number(prompt('Введите количество баллов по информатике'));
+const mat = Number(prompt('Введите количество баллов по математике'));
+const rus = Number(prompt('Введите количество баллов по русскому языку'));
+const info = Number(prompt('Введите количество баллов по информатике'));
 
-// const sum = mat + rus + info;
+const sum = mat + rus + info;
 
-// if (sum >= 265) {
-//    console.log('Поздравляю, вы поступили на бюджет!');
-// }
-// else {
-//    console.log('Попробуйте в следующий раз');
-// }
+if (sum >= 265) {
+   console.log('Поздравляю, вы поступили на бюджет!');
+}
+else {
+   console.log('Попробуйте в следующий раз');
+}
 
 // Банкомат
 const cash = Number(prompt('Сколько денег вы хотите снять?'))
@@ -28,5 +30,5 @@ const cash = Number(prompt('Сколько денег вы хотите снят
 if (cash % 100 === 0) {
    console.log(`Вы можете снять ${cash} рублей`);
 } else {
-   console.log('К сожалению вы можете снять сумму кратную ста рублям');
+   console.log('К сожалению вы можете снять только сумму кратную ста рублям');
 }
